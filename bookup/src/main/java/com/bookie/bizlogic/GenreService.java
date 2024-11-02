0c483dff64b9bc2f4840f2d8229417ebb78b7819
Tanya Reuben <tanyareuben@gmail.com>
2024-11-01 17:50:32 -0700

    Genre entity files added

---
+++ bookup/src/main/java/com/bookie/bizlogic/GenreService.java
@@ -0,0 +1,34 @@
+package com.bookie.bizlogic;
+
+import java.util.List;
+
+import com.bookie.dao.GenreDAO;
+import com.bookie.models.Genre;
+
+public class GenreService {
+
+private GenreDAO genreDAO;
+	
+	public GenreService() {
+		genreDAO = new GenreDAO();
+	}
+	
+	public List<Genre> getGenres() {
+
+		return genreDAO.getAllGenres();
+	}
+	
+	//TODO Add Admin check, only admin can use this method
+	public boolean addGenre(Genre genre) {
+		
+		return genreDAO.add(genre);
+	}
+	
+	//TODO Add Admin check, only admin can use this method
+	public boolean changeGenreName(int genreID, String newName) {
+		Genre a = genreDAO.getById(genreID);
+		a.setName(newName);
+		return genreDAO.update(a);
+	}
+	
+}
---
+++ bookup/src/main/java/com/bookie/dao/GenreDAO.java
@@ -0,0 +1,103 @@
+package com.bookie.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bookie.models.Genre;
+
+public class GenreDAO extends BaseDAO<Genre, Integer> {
+
+	@Override
+	public Genre getById(Integer genreID) {
+		Genre genre = null; 
+		try {
+			String query = "SELECT * FROM Genres WHERE genreID = ?";
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setInt(1, genreID);
+			ResultSet rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+                genre = new Genre(
+                    rs.getInt(genreID),
+                    rs.getString("name")
+                );
+            }
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return genre;
+	}
+
+	@Override
+	public boolean add(Genre genre) {
+		try {
+			String query = "INSERT INTO Genres (genreID, name) VALUES (?, ?)";
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setInt(1, genre.getGenreID());
+			stmt.setString(2, genre.getName());
+			
+			return stmt.executeUpdate() > 0;
+			
+		} catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+	}	
+	
+
+	@Override
+	public boolean update(Genre genre) {
+		try {
+            String query = "UPDATE Genres SET name = ? WHERE genreID = ?";    
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, genre.getName());
+            stmt.setInt(2, genre.getGenreID());
+
+            return stmt.executeUpdate() > 0;
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+	}
+	
+
+	@Override
+	public boolean delete(Integer genreID) {
+		try {
+            String query = "DELETE FROM Genres WHERE genreID = ?";    
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, genreID);
+            return stmt.executeUpdate() > 0;
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+	}
+	
+	public List<Genre> getAllGenres() {
+        List<Genre> genres = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM Genres";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Genre genre = new Genre(
+                    rs.getInt("genreID"),
+                    rs.getString("name")
+                );
+                genres.add(genre);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return genres;
+    }
+
+}
